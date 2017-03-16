6baa59f11e1dfe062c8aa3b2c10760851340cb51
YorkShen <shenyuancs@gmail.com>
2017-03-16 21:40:07 +0800

    Optimize 23, using divide and conquer method

--- python/23.py
+++ python/23.py
@@ -4,37 +4,36 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution(object):
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        ret = ListNode(None)
-        lists = filter(None, lists)
-        if lists:
-            self.__mergeKlists(lists, ret)
-        return ret.next
-
-    def __mergeKlists(self, lists, pointer):
-        while len(filter(None, lists)) > 1:
-            min_node_index = self.__min_node_index(lists)
-            min_node = lists[min_node_index]
-            pointer.next = min_node
-            pointer = pointer.next
-            lists[min_node_index] = min_node.next
-            lists = filter(None, lists)
-        if lists:
-            pointer.next = lists[0]
-
-    def __min_node_index(self, lists):
-        min_value = lists[0].val
-        min_index = 0
-        for index, node in enumerate(lists):
-            if node.val < min_value:
-                min_value = node.val
-                min_index = index
-        return min_index
+        lists_len = len(lists)
+        if lists_len == 0:
+            return None
+        elif lists_len == 1:
+            return lists[0]
+        else:
+            list_a = self.mergeKLists(lists[:lists_len/2])
+            list_b = self.mergeKLists(lists[lists_len/2:])
+            ret = ListNode(None)
+            pointer = ret
+            while list_a and list_b:
+                if list_a.val < list_b.val :
+                    pointer.next = list_a
+                    list_a = list_a.next
+                else:
+                    pointer.next = list_b
+                    list_b = list_b.next
+                pointer = pointer.next
+            if list_a:
+                pointer.next = list_a
+            elif list_b:
+                pointer.next = list_b
+            return ret.next
 
 
 class ListNode(object):
@@ -44,7 +43,7 @@ class ListNode(object):
 
 
 s = Solution()
-ret = s.mergeKLists([None])
-while ret:
-    print ret.val
-    ret = ret.next
+result = s.mergeKLists([None])
+while result:
+    print result.val
+    result = result.next
