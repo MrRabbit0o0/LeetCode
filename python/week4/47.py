df1d1228ac39e6bea9f8a610ac56f3c056cfedf7
YorkShen <shenyuancs@gmail.com>
2017-03-29 23:33:23 +0800

    Beats 92%

--- python/week4/47.py
+++ python/week4/47.py
@@ -1,30 +1,19 @@
-from collections import Counter
-
-
 class Solution(object):
     def permuteUnique(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        counter = dict(Counter(nums))
-        ret = []
-        self.__permute(counter, len(nums), ret, [])
+        ret = [[]]
+        for num in nums:
+            temp = []
+            for possible in ret:
+                for position in xrange(len(possible) + 1):
+                    temp.append(possible[:position] + [num] + possible[position:])
+                    if position < len(possible) and num == possible[position]:
+                        break
+            ret = temp
         return ret
-
-    def __permute(self, counter, length, ret, temp):
-        if len(temp) == length:
-            ret.append(temp[:])
-        else:
-            for key, value in dict(counter).items():
-                temp.append(key)
-                if value > 1:
-                    counter[key] = value - 1
-                else:
-                    del counter[key]
-                self.__permute(counter, length, ret, temp)
-                temp.pop()
-                counter[key] = value
 
 
 s = Solution()
