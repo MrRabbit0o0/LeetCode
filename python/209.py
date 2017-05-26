2bfe779b0fd4d562bfaeab6cb4016d3940d8831b
MrRabbit0o0 <svinzfstd@gmail.com>
2017-05-26 23:01:30 +0800

    naive

--- python/209.py
+++ python/209.py
@@ -1,29 +1,33 @@
 # coding: utf8
 
+
 class Solution(object):
-    def containsNearbyDuplicate(self, nums, k):
+    def minSubArrayLen(self, s, nums):
         """
+        :type s: int
         :type nums: List[int]
-        :type k: int
-        :rtype: bool
+        :rtype: int
         """
-        num_idx = {}
-        for i in range(len(nums)):
-            n = nums[i]
-            if n not in num_idx:
-                num_idx[n] = []
-            for idx in num_idx[n]:
-                if i - idx <= k:
-                    return True
-            num_idx[n].append(i)
-        return False
+        sum_list = []
+        sum_n = 0
+        min_length = len(nums) + 1
+        for n in nums:
+            sum_n += n
+            sum_list.append(n)
+            if sum_n >= s:
+                while sum_list and sum_n >= s:
+                    sub_n = sum_list.pop(0)
+                    sum_n -= sub_n
+                min_length = min(min_length, len(sum_list)+1)
+        return min_length if min_length <= len(nums) else 0
 
 
 if __name__ == '__main__':
-    nums = []
-    k = 0
-    print Solution().containsNearbyDuplicate(nums, k)
-    nums = [-1, -1]
-    k = 1
-    print Solution().containsNearbyDuplicate(nums, k)
-
+    s = 7
+    nums = [2, 3, 1, 2, 4, 3]
+    print s, nums
+    print Solution().minSubArrayLen(s, nums)
+    s = 0
+    nums = [1, 4, 4]
+    print s, nums
+    print Solution().minSubArrayLen(s, nums)
