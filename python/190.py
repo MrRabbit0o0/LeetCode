63ade67c31c5050643a86fd54c48ad19a56e159f
MrRabbit0o0 <svinzfstd@gmail.com>
2017-06-01 02:17:02 +0800

    bit manipulation

--- python/190.py
+++ python/190.py
@@ -1,13 +1,19 @@
 # coding: utf8
 
 class Solution(object):
-    def hammingWeight(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        total = 0
-        for _ in range(32):
-            total += n % 2
-            n /= 2
-        return total
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        n = (n >> 16) | (n << 16)
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8)
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4)
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2)
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1)
+        return n
+
+
+if __name__ == '__main__':
+    n = 43261596
+    print n
+    print Solution().reverseBits(n)
+
