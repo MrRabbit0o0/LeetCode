970e0f4c944ca46c1c2e98afb3794dca72b1322b
MrRabbit0o0 <svinzfstd@gmail.com>
2017-07-23 01:55:09 +0800

    naive

---
+++ python/378.py
@@ -0,0 +1,13 @@
+# coding: utf8
+
+
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        return sorted([ele for row in matrix for ele in row])[k-1]
+
+
